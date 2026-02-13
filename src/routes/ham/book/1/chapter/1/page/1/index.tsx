6b7a7b30a04b781c7466280563a540dd6072a0cd
fatherbrennan <bjt.lepore@gmail.com>
2026-02-13 15:33:39 +1030

    build: :rocket: experiment ssg routing

--- src/routes/ham/book/1/chapter/1/page/1/index.tsx
+++ src/routes/ham/book/1/chapter/1/page/1/index.tsx
@@ -1,11 +1,13 @@
-import Rose from '$app/assets/rose.svg?jsx';
 import { APP_TITLE, OpenGraph } from '$app/constants';
+import { content } from '$app/routes/ham/book/content';
+import { withAppTitle } from '$app/utils/title';
 import { openGraphMeta } from '$app/utils/url';
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import { useLocation } from '@builder.io/qwik-city';
 
 const title = APP_TITLE;
-const description = 'TODO: Needs a description';
+const description = withAppTitle('ham');
 
 export const head: DocumentHead = {
   meta: [
@@ -17,9 +19,14 @@ export const head: DocumentHead = {
 };
 
 export default component$(() => {
+  const { params } = useLocation();
+
   return (
     <div>
-      <Rose />
+      <h1>{content[0].title}</h1>
+      <h1>{content[0].title}</h1>
+      <pre>{JSON.stringify(params, null, 2)}</pre>
+      <pre>{JSON.stringify(content, null, 2)}</pre>
     </div>
   );
 });
---
+++ src/routes/ham/book/content.ts
@@ -0,0 +1,11 @@
+export const content = [
+  {
+    chapters: [
+      {
+        pages: [{ title: 'Page 1' }],
+        title: 'Chapter 1',
+      },
+    ],
+    title: 'Book 1',
+  },
+];
