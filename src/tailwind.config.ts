f207b830e46043503ce58f509c6231f625d57170
fatherbrennan <bjt.lepore@gmail.com>
2026-03-12 16:50:58 +1030

    fix: :ambulance: add tailwind styles

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -2,6 +2,6 @@ import tailwindCssForms from '@tailwindcss/forms';
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./**/*.{ts,tsx}'],
+  content: ['./**/*.{ts,svelte}'],
   plugins: [tailwindCssForms({ strategy: 'base' })],
 } satisfies Config;
